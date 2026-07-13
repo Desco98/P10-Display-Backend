543f406d8893f71e18c099cc44973c73019955d8
Desmond Somtochukwu <dsomto98@gmail.com>
2026-07-13 23:02:17 +0100

    updated requirements.txt

--- core/config.py
+++ core/config.py
@@ -1,12 +1,11 @@
 from pydantic_settings import BaseSettings
-from typing import Optional
 
 class Settings(BaseSettings):
-    MQTT_BROKER: str = "localhost"
-    MQTT_PORT: int = 1883
-    MQTT_USERNAME: Optional[str] = None
-    MQTT_PASSWORD: Optional[str] = None
-    MQTT_USE_TLS: bool = False
+    MQTT_BROKER: str
+    MQTT_PORT: int
+    MQTT_USERNAME: str
+    MQTT_PASSWORD: str
+    MQTT_USE_TLS: bool
 
     class Config:
         env_file = ".env"
